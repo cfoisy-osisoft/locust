0ce023b5322989a91e95c41d6d750b1682f64b57
Christian Foisy <cfoisy@osisoft.com>
2019-12-02 15:31:09 +0000

    Test 10-building hackdavis

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,22 @@
 FROM python:3.6-alpine as builder
 
-RUN apk --no-cache add g++ zeromq-dev libffi-dev
+RUN apk --no-cache add g++ zeromq-dev libffi-dev 
 COPY . /src
 WORKDIR /src
 RUN pip install .
+RUN pip install -i https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple ocs-hackdavis==0.10.0
+RUN pip uninstall -y ocs-sample-library-preview
+RUN pip install -i https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple ocs-sample-library-preview-hub==0.0.36rc0 
+RUN pip install pyyaml
+RUN pip list
 
 FROM python:3.6-alpine
 
-RUN apk --no-cache add zeromq && adduser -s /bin/false -D locust
+RUN apk --no-cache add zeromq wget && adduser -s /bin/false -D locust
 COPY --from=builder /usr/local/lib/python3.6/site-packages /usr/local/lib/python3.6/site-packages
 COPY --from=builder /usr/local/bin/locust /usr/local/bin/locust
 COPY docker_start.sh docker_start.sh
+COPY locustfile.py locustfile.py
 RUN chmod +x docker_start.sh
 
 EXPOSE 8089 5557 5558
--- docker_start.sh
+++ docker_start.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env sh
 
-if [ -z "${TARGET_URL}" ]; then
-  echo "ERROR: TARGET_URL not configured" >&2
-  exit 1
-fi
+# if [ -z "${TARGET_URL}" ]; then
+#  echo "ERROR: TARGET_URL not configured" >&2
+#  exit 1
+# fi
 
 LOCUST_MODE="${LOCUST_MODE:=standalone}"
-_LOCUST_OPTS="-f ${LOCUSTFILE_PATH:-/locustfile.py} -H ${TARGET_URL}"
+_LOCUST_OPTS="-f ${LOCUSTFILE_PATH:-./locustfile.py} -H ${TARGET_HOST}"
 
 if [ "${LOCUST_MODE}" = "master" ]; then
     _LOCUST_OPTS="${_LOCUST_OPTS} --master"
-elif [ "${LOCUST_MODE}" = "slave" ]; then
-    if [ -z "${LOCUST_MASTER_HOST}" ]; then
-        echo "ERROR: MASTER_HOST is empty. Slave mode requires a master" >&2
+elif [ "${LOCUST_MODE}" = "worker" ]; then
+    if [ -z "${LOCUST_MASTER}" ]; then
+        echo "ERROR: LOCUST_MASTER is empty. Slave mode requires a master" >&2
         exit 1
     fi
+    LOCUST_MASTER_HOST=${LOCUST_MASTER}
+    LOCUST_MASTER=no
+    while ! wget --spider -qT5 ${LOCUST_MASTER_HOST}:8089 >/dev/null 2>&1; do
+        echo "Waiting for master"
+	sleep 5
+    done
 
     _LOCUST_OPTS="${_LOCUST_OPTS} --slave --master-host=${LOCUST_MASTER_HOST} --master-port=${LOCUST_MASTER_PORT:-5557}"
 fi
